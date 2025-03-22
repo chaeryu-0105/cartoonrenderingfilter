951cbd308b5692ee11a6e3a60e724ea9924b320d
guruem-0105 <gureum0227@gmail.com>
2025-03-22 09:39:07 +0900

    test

--- cartoonrenderingfilter.py
+++ cartoonrenderingfilter.py
@@ -2,8 +2,8 @@ import cv2 as cv
 import numpy as np
 
 
-img = cv.imread('img.jpg')
-grayimg = cv.imread('img.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.imread('img2.jpg')
+grayimg = cv.imread('img2.jpg', cv.IMREAD_GRAYSCALE)
 grayimg = cv.medianBlur(grayimg, 3)
 maskimg = cv.adaptiveThreshold(grayimg, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 7, 3)
 maskimg = cv.cvtColor(maskimg, cv.COLOR_GRAY2BGR)
